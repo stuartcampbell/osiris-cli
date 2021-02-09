72420b0f90eabdbda333cd0b5840276d0b24a990
Stuart Campbell <scampbell@bnl.gov>
2021-02-08 22:01:45 -0500

    Added version

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+from typing import Optional
+
 import typer
 
 import data_session
 import instruments
 import users
 
+__version__ = 0.1
+
 app = typer.Typer()
 app.add_typer(users.app, name="users")
 app.add_typer(instruments.app, name="instruments")
 app.add_typer(data_session.app, name="data-session")
 
-if __name__ == "__main__":
-    app()
 
+def version_callback(value: bool):
+    if value:
+        typer.echo(f"Awesome CLI Version: {__version__}")
+        raise typer.Exit()
+
+
+@app.command()
+def main(
+        version: Optional[bool] = typer.Option(
+            None, "--version", callback=version_callback, is_eager=True
+        ),
+):
+    pass
+
+
+if __name__ == "__main__":
+    typer.run(main)
